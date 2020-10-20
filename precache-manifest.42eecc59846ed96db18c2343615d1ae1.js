04fee7ac8c4154c0ca4cb72315b42c5d8e04e2b8
Artem Perevezii <perevesiy@gmail.com>
2020-10-20 15:38:22 +0300

    Updates

---
+++ precache-manifest.42eecc59846ed96db18c2343615d1ae1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a93b31f3879f80d9a80813b90c858217",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2db4db3b511354e63cac",
+    "url": "/static/css/2.0939e70c.chunk.css"
+  },
+  {
+    "revision": "cc3b2e0fc25b84bf8b78",
+    "url": "/static/css/main.32de93a5.chunk.css"
+  },
+  {
+    "revision": "2db4db3b511354e63cac",
+    "url": "/static/js/2.a2c83bbe.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.a2c83bbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc3b2e0fc25b84bf8b78",
+    "url": "/static/js/main.48275e79.chunk.js"
+  },
+  {
+    "revision": "525225d6c3bcd26625d5",
+    "url": "/static/js/runtime-main.f502e83e.js"
+  },
+  {
+    "revision": "dd79bc92d5144d620b4c5af213b0632f",
+    "url": "/static/media/logo.dd79bc92.png"
+  },
+  {
+    "revision": "d3f17edfdcf327a7897d357214b5a721",
+    "url": "/static/media/overlay1.d3f17edf.png"
+  },
+  {
+    "revision": "cfd27743fa920273c4bee3676bf0e7af",
+    "url": "/static/media/overlay2.cfd27743.png"
+  },
+  {
+    "revision": "4133c267843273d57c9707ae0aa04091",
+    "url": "/static/media/overlay3.4133c267.png"
+  },
+  {
+    "revision": "061367e062ebad3dc1f69011b9a2f96e",
+    "url": "/static/media/overlay4.061367e0.png"
+  }
+]);
